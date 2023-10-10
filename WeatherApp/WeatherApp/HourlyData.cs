460815b152c13a25b3299a6100a3cd3a231b6f6f
McGowanJ <113394897+McGowanJericho@users.noreply.github.com>
2023-10-10 13:23:05 -0400

    Add files via upload

---
+++ WeatherApp/WeatherApp/HourlyData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public class HourlyData
+    {
+        public List<string> time { get; set; }
+        public List<double> temperature_2m { get; set; }
+  
+    }
+}
